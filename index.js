f3f7e778884da6e8d7ef0c67ab6a27844766330d
Candid Dauth <cdauth@cdauth.eu>
2013-01-14 02:53:00 +0100

    Bugfixing

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var keyring = require("./lib/keyring");
-
-module.exports = {
-	getKeyring : keyring.getKeyring,
-	initialiseDatabase : keyring.initialiseDatabase,
-	_KeyringPostgres : keyring._KeyringPostgres
-};
+module.exports = require("./lib/keyring");
